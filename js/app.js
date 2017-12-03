4a82da98d91b39691f641444a1104f7024d6aea8
AraceliGS <aragutarras@gmail.com>
2017-12-03 17:55:11 -0500

    añadiendo evento click a las imágenes de mi documento HTML

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 var personajes = document.querySelectorAll('.personaje');
 for (var i = 0; i < personajes.length; i++) {
-  personajes[i].addEventListener('click', mostrarInfoPersonaje);    
+  personajes[i].addEventListener('click', mostrarInfoPersonaje);
+}
+
+function mostrarInfoPersonaje() {
+  var nombre = this.dataset.nombre; // Dataset es un método
+  var fecha = this.dataset.fecha;
+
+  var parrafoInfo = document.createElement('p');
+  parrafoInfo.classList.add('active');
+  parrafoInfo.innerText = 'Su nombre es ' + nombre + ' y su primera aparición fue en ' + fecha;
+
+  var espacio = document.querySelector('.informacion');
+  espacio.appendChild(parrafoInfo);
+
+  var infoVisible = document.querySelector('p.active');
+  infoVisible.classList.remove('active');
 }
